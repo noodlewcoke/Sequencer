feb3f7ebca067273fe361ed4353d1ff628c7ab60
noodlewcoke <isouzd@gmail.com>
2018-05-14 14:34:39 +0300

    .

--- sequencer.py
+++ sequencer.py
@@ -1,2 +1,30 @@
 import numpy as np 
+from sympy.utilities.iterables import variations
 
+class sequencer:
+
+    def __init__(self, sequence_lengths=1, depth=1, sparsity=None, discontiguity_lenghts=0):
+        self.sequence_lengths = sequence_lengths
+        self.depth = depth
+        self.sparsity = sparsity
+        self.discontiguity_lenghts = discontiguity_lenghts
+
+    def uniform_sequence(self, len_data, n_class):
+        pass
+
+
+    def create_dataset(self, data, label_dim, save=None):
+        pass
+
+def test(num_labels, sequence_lengths):
+    labels = np.arange(num_labels)
+    sequences = []
+    for r in sequence_lengths:
+        for i in list(variations(labels, r, repetition=True)):
+            sequences.append(i) 
+    return sequences
+
+
+if __name__ == '__main__':
+    seq = sequencer(sequence_lengths=5)
+    print(len(test(10, [3])))
