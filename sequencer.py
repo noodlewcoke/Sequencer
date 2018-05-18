d1e36dd4caf2b073026eab3bbf6cd75f60f7ce8c
noodlewcoke <isouzd@gmail.com>
2018-05-18 18:21:00 +0300

    Successful limited_sequence()

--- sequencer.py
+++ sequencer.py
@@ -1,10 +1,12 @@
 import numpy as np 
 from sympy.utilities.iterables import variations
-
+import itertools
+from random import shuffle
 
 #TODO: Exceptions :: sequence_lengths != 0 or [2,2]
 #TODO: Returning a sequence map :: e.g. histogram
 #TODO: Sequence types: entropy, advancing, combination
+
 class sequencer:
 
     def __init__(self, sequence_lengths=[1], diversity=None, discontiguity_lenghts=0):
@@ -14,15 +16,25 @@ class sequencer:
 
     def limited_sequence(self, num_labels, upper_bounds):
         depth = len(upper_bounds)
-        labels = np.arange(num_labels)
+        unseq = np.arange(num_labels)
         sequences = []
         for d in range(depth):
             for r in self.sequence_lengths:
-                for i in list(variations(range(len(labels)), r, repetition=True)):
-                    sequences.append(i)
-            labels = labels[np.random.permutation(sequences)[:upper_bounds[d]]]
+                indices = np.random.randint(0, len(unseq), size=(upper_bounds[d], r))
+                for i in unseq[indices]:
+                    if d:
+                        temp = [list(a) for a in i]
+                        sequences.append(sum(temp,[]))
+                    else:
+                        sequences.append(list(i))
+
+            unseq = np.random.permutation(sequences)
+            
+            # shuffle(sequences)
+            # unseq = np.array(sequences.copy())
             sequences = []
-        return labels
+        unseq = [list(a) for a in unseq]
+        return sum(unseq,[])
 
     def time_invariant_seq(self, len_data, n_class):
         pass
@@ -30,30 +42,10 @@ class sequencer:
     def create_dataset(self, data, label_dim, save=None):
         pass
 
-def test(num_labels, sequence_lengths, upper_bounds):
-
-    depth = len(upper_bounds)
-    labels = np.arange(num_labels)
-    sequences = []
-    for r in sequence_lengths:
-        for i in list(variations(labels, r, repetition=True)):
-            sequences.append(i) 
-    base = np.random.permutation(sequences)[:upper_bounds[0]]
-
-    sequences = []   
-    for r in sequence_lengths:
-        for i in list(variations(base, r, repetition=True)):
-            sequences.append(i)
-    
-
-    
-    return np.random.permutation(sequences)[:upper_bounds[1]]
-    # return np.concatenate([*base], axis=0)
 
 
 if __name__ == '__main__':
-    sequence = sequencer(sequence_lengths=[2])
-    seq = sequence.limited_sequence(10, [100,50,10])
-    # seq = test(4, [2,3], [25, 5])
-    # seqt = set([tuple(set(s)) for s in seq])
+    sequence = sequencer(sequence_lengths=[3,5])
+    seq = sequence.limited_sequence(10, [100,100,100, 100])
     print(seq)
+    print(len(seq))
